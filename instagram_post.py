09e2ecb9e3d43af616c86e73f2f40f9408adbcf7
Mythesis002 <131542998+Mythesis002@users.noreply.github.com>
2025-04-08 20:20:08 +0530

    Update instagram_post.py

--- instagram_post.py
+++ instagram_post.py
@@ -3,5 +3,5 @@ import os
 print("Current working directory:", os.getcwd())
 print("Files in this directory:", os.listdir())
 
-VIDEO_PATH = "promo.mp4"
+VIDEO_PATH = "video.MOV"
 print("File exists:", os.path.exists(VIDEO_PATH))
